c4d4ca69c0462625eb4b09d1a1a50eb0e5a2a36e
kajackdfw <kajackdfw@yahoo.com>
2019-06-07 09:06:13 -0500

    pin mapping questions answered

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -39,23 +39,21 @@
   #define X_STEP_BIT    2 // MEGA2560 Digital Pin 24
   #define Y_STEP_BIT    3 // MEGA2560 Digital Pin 25
   #define Z_STEP_BIT    4 // MEGA2560 Digital Pin 26
-  #define A_STEP_BIT    5 // MEGA2560 Digital Pin FIXME how to connect this to pin 27 ?
-  #define B_STEP_BIT    6 // MEGA2560 Digital Pin FIXME how to connect this to pin 28 ?
-  #define C_STEP_BIT    7 // MEGA2560 Digital Pin FIXME how to connect this to pin 29 ?
-  // #define H_STEP_BIT    8 // MEGA2560 Digital Pin , we ran out of bits, so hard wire H axis to X axis if needed
+  #define A_STEP_BIT    5 // MEGA2560 Digital Pin 27
+  #define B_STEP_BIT    6 // MEGA2560 Digital Pin 28
+  #define C_STEP_BIT    7 // MEGA2560 Digital Pin 29
   #define STEP_MASK ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)|(1<<A_STEP_BIT)|(1<<B_STEP_BIT)|(1<<C_STEP_BIT)) // All step bits
 
   // Define step direction output pins. NOTE: All direction pins must be on the same port.
-  #define DIRECTION_DDR     DDRC
+  #define DIRECTION_DDR     DDRC // analog pins
   #define DIRECTION_PORT    PORTC
   #define DIRECTION_PIN     PINC
   #define X_DIRECTION_BIT   7 // MEGA2560 Digital Pin 30
   #define Y_DIRECTION_BIT   6 // MEGA2560 Digital Pin 31
   #define Z_DIRECTION_BIT   5 // MEGA2560 Digital Pin 32
-  #define A_DIRECTION_BIT   4 // MEGA2560 Digital Pin FIXME how to connect this to pin 33 ?
-  #define B_DIRECTION_BIT   3 // MEGA2560 Digital Pin FIXME how to connect this to pin 34 ?
-  #define C_DIRECTION_BIT   2 // MEGA2560 Digital Pin FIXME how to connect this to pin 35 ?
-  //#define H_DIRECTION_BIT   6 // MEGA2560 Digital Pin , we ran out of bits, so hard wire H axis to X axis if needed
+  #define A_DIRECTION_BIT   4 // MEGA2560 Digital Pin 33
+  #define B_DIRECTION_BIT   3 // MEGA2560 Digital Pin 34
+  #define C_DIRECTION_BIT   2 // MEGA2560 Digital Pin 35
   #define DIRECTION_MASK ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)|(1<<A_DIRECTION_BIT)|(1<<B_DIRECTION_BIT)|(1<<C_DIRECTION_BIT)) // All direction bits FIXME
 
   // Define stepper driver enable/disable output pin.
@@ -66,12 +64,12 @@
 
   // Define homing/hard limit switch input pins and limit interrupt vectors. 
   // NOTE: All limit bit pins must be on the same port
-  #define LIMIT_DDR       DDRB
+  #define LIMIT_DDR       DDRB // pins 8 to 13 see https://www.arduino.cc/en/Reference/PortManipulation
   #define LIMIT_PORT      PORTB
   #define LIMIT_PIN       PINB
-  #define A_LIMIT_BIT     1 // MEGA2560 Digital FIXME wich pin ?
-  #define B_LIMIT_BIT     2 // MEGA2560 Digital FIXME wich pin ?
-  #define C_LIMIT_BIT     3 // MEGA2560 Digital FIXME wich pin ?
+  #define A_LIMIT_BIT     1 // MEGA2560 Digital Pin 7
+  #define B_LIMIT_BIT     2 // MEGA2560 Digital Pin 8
+  #define C_LIMIT_BIT     3 // MEGA2560 Digital Pin 9
   #define X_LIMIT_BIT     4 // MEGA2560 Digital Pin 10
   #define Y_LIMIT_BIT     5 // MEGA2560 Digital Pin 11
   #define Z_LIMIT_BIT     6 // MEGA2560 Digital Pin 12
@@ -144,6 +142,7 @@
 
 #endif
 
+// I am not using Ramps, so ignore this block
 #ifdef CPU_MAP_2560_RAMPS_BOARD // (Arduino Mega 2560) with Ramps 1.4 Board
   #include "nuts_bolts.h"
 
